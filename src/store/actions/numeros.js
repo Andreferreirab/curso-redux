26811b9f11f18bbdcbe84ead932bc317de2a495f
Andre Ferreira <andreferreirab@gmail.com>
2020-07-29 08:20:58 -0300

    Codigo refatorado

--- src/store/actions/numeros.js
+++ src/store/actions/numeros.js
@@ -1,15 +1,16 @@
+import { NUM_MAX_ALTERAR, NUM_MIN_ALTERAR } from "./actionTypes";
 
 // Action Creator
 export function alterarNumeroMinimo(novoNumero) {
     return {
-        type: 'NUM_MIN_ALTERAR',
+        type: NUM_MIN_ALTERAR,
         payload: novoNumero
     }
 }
 
 export function alterarNumeroMaximo(novoNumero) {
     return {
-        type: 'NUM_MAX_ALTERAR',
+        type: NUM_MAX_ALTERAR,
         payload: novoNumero
     }
 }
